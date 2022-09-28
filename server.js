6d4946bd7262eaf21283fc09a480b62736a3c51a
jwatts41 <wattsjonovan@yahoo.com>
2022-09-28 12:03:38 -0400

    changes

--- server.js
+++ server.js
@@ -6,17 +6,12 @@ const PORT = process.env.PORT
 const app = express()
 // console.log(PORT)
 
-//ROUTES
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
+  res.send('Welcome to an Awesome App about Breads!')
 })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
 
 // LISTEN
 app.listen(PORT, () => {
-    console.log('listening on port', PORT)
+  console.log('listening on port', PORT);
 })
